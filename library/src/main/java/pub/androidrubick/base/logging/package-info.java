b0d441b066399374576dcb343d26d55bee0c3d8b
yong01.yin <yong01.yin@vipshop.com>
2017-11-27 11:26:15 +0800

    add java utils code

---
+++ library/src/main/java/pub/androidrubick/base/logging/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * 统一控制APP中的日志开关，可以使用{@link pub.androidrubick.base.logging.ARLogger}。等等...
+ * 
+ * @author Yin Yong
+ */
+package pub.androidrubick.base.logging;
---
+++ library/src/main/java/pub/androidrubick/base/utils/Exceptions.java
@@ -0,0 +1,24 @@
+package pub.androidrubick.base.utils;
+
+/**
+ * <p/>
+ * <p/>
+ * Created by Yin Yong on 15/7/2.
+ *
+ * @since 1.0
+ */
+public class Exceptions {
+    private Exceptions() { }
+
+    public static RuntimeException asRuntime(Throwable e) {
+        if (e instanceof RuntimeException) {
+            return (RuntimeException) e;
+        }
+        return new DummyRuntimeException(e);
+    }
+
+    public static RuntimeException asRuntime(String msg) {
+        return new RuntimeException(msg);
+    }
+
+}
